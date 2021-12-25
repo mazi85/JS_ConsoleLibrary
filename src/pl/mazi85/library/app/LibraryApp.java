1ec48cfec1349e81fe80f3b65c9f4307add3f803
Adrian Mazur <muzy1125@gmail.com>
2021-12-25 18:24:41 +0100

    Static components

--- src/pl/mazi85/library/app/LibraryApp.java
+++ src/pl/mazi85/library/app/LibraryApp.java
@@ -2,10 +2,11 @@ package pl.mazi85.library.app;
 
 public class LibraryApp {
 
+    private static final String APP_NAME = "Biblioteka v1.0";
     public static void main(String[] args) {
 
-        final String appName = "Biblioteka v0.9";
-        System.out.println(appName);
+
+        System.out.println(APP_NAME);
         LibraryControl libraryControl = new LibraryControl();
         libraryControl.controlLoop();
 
