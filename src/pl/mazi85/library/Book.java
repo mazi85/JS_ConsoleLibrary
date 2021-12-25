47d0e5bed1b5a1b94429f6bb0b1a8ebb1c3669e2
Adrian Mazur <muzy1125@gmail.com>
2021-12-25 12:15:47 +0100

    Methods overloading and this

--- src/pl/mazi85/library/Book.java
+++ src/pl/mazi85/library/Book.java
@@ -2,12 +2,16 @@ package pl.mazi85.library;
 
 public class Book {
 
-    public Book(String title, String author, int releaseDate, int pages, String publisher, String isbn) {
+    public Book(String title, String author, int releaseDate, int pages, String publisher) {
         this.title = title;
         this.author = author;
         this.releaseDate = releaseDate;
         this.pages = pages;
         this.publisher = publisher;
+    }
+
+    public Book(String title, String author, int releaseDate, int pages, String publisher, String isbn) {
+       this(title,author,releaseDate,pages,publisher);
         this.isbn = isbn;
     }
 
--- src/pl/mazi85/library/Library.java
+++ src/pl/mazi85/library/Library.java
@@ -4,12 +4,12 @@ public class Library {
 
     public static void main(String[] args) {
 
-        final String appName = "Biblioteka v0.3";
+        final String appName = "Biblioteka v0.4";
 
         Book book1 = new Book("W pustyni i w puszczy","Henryk Sienkiewicz",2010,296,"Greg","9788373271890");
         Book book2 = new Book("JAVA. Efektywne programowanie.Wydanie II","Joshua Bloch",2009,352,"Helion","9788324620845D");
         Book book3 = new Book("SCJP Sun Certified Programmer for JAVA 6 Study Guide","Bert Bates, Katherine Sierra",2008,851,
-                "McGraw-Hill Osborne Media","9780071591065");
+                "McGraw-Hill Osborne Media");
 
         System.out.println(appName);
         System.out.println("Książki dostępne w bibliotece: ");
