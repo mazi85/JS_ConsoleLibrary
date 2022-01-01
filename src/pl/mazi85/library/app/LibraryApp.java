baac079c0ddac9c2788d15b2bab72fdae34a003d
Adrian Mazur <muzy1125@gmail.com>
2022-01-01 12:00:55 +0100

    Inner Class

--- src/pl/mazi85/library/app/LibraryApp.java
+++ src/pl/mazi85/library/app/LibraryApp.java
@@ -2,7 +2,7 @@ package pl.mazi85.library.app;
 
 public class LibraryApp {
 
-    private static final String APP_NAME = "Biblioteka v1.2";
+    private static final String APP_NAME = "Biblioteka v1.6";
     public static void main(String[] args) {
 
 
--- src/pl/mazi85/library/app/LibraryControl.java
+++ src/pl/mazi85/library/app/LibraryControl.java
@@ -111,4 +111,45 @@ public class LibraryControl {
         }
     }
 
+    private enum Option {
+        EXIT(0, "wyjście z programu"),
+        ADD_BOOK(1, "dodanie nowej książki"),
+        ADD_MAGAZINES(2, "dodanie nowego magazynu"),
+        PRINT_BOOKS(3, "wyświetl dostępne książki"),
+        PRINT_MAGAZINES(4, "wyświetl dostępne magazyny");
+
+
+        private final int number;
+        private final String description;
+
+
+        Option(int number, String description) {
+            this.number = number;
+            this.description = description;
+        }
+
+
+        public int getNumber() {
+            return number;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        @Override
+        public String toString() {
+            return number + " - " + description;
+        }
+
+        public static Option crateFromInt(int option) throws NoSuchOptionException {
+
+            try {
+                return Option.values()[option];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new NoSuchOptionException("Brak opcji o id " + option);
+            }
+        }
+    }
+
 }
