fa622ce35760af0c6583c7ae3c52f4e7de079907
Adrian Mazur <muzy1125@gmail.com>
2022-02-06 09:53:31 +0100

    Stream from Collection

--- src/pl/mazi85/library/io/ConsolePrinter.java
+++ src/pl/mazi85/library/io/ConsolePrinter.java
@@ -1,23 +1,20 @@
 package pl.mazi85.library.io;
 
-import pl.mazi85.library.model.Book;
-import pl.mazi85.library.model.LibraryUser;
-import pl.mazi85.library.model.Magazine;
-import pl.mazi85.library.model.Publication;
+import pl.mazi85.library.model.*;
 
 import java.util.Collection;
 
 public class ConsolePrinter {
 
     public void printBooks(Collection<Publication> publications){
-        int countBook=0;
 
-        for (Publication publication: publications) {
-            if (publication instanceof Book){
-                printLine(publication.toString());
-                countBook++;}
-        }
-        if (countBook==0){
+        long count = publications.stream()
+                .filter(p -> p instanceof Book)
+                .map(Publication::toString)
+                .peek(this::printLine)
+                .count();
+
+        if (count==0){
             printLine("Brak książek w bibliotece");
         }
 
@@ -38,10 +35,10 @@ public class ConsolePrinter {
     }
 
     public void printUsers (Collection<LibraryUser> users){
-        for (LibraryUser user : users) {
-            printLine(user.toString());
+        users.stream()
+                .map(User::toString)
+                .forEach(this::printLine);
 
-        }
     }
 
     public void printLine(String string){
